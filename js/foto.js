d4739120f4683a77cfdba10b01f545542c487b26
Natali-ok2 <84032882+Natali-ok2@users.noreply.github.com>
2021-05-13 17:42:02 +0300

    Add files via upload
    Natali

---
+++ js/foto.js
@@ -0,0 +1,6 @@
+function DoIMG(){
+	document.getElementById('IdIMG').style.width='100px';
+	document.getElementById('IdIMG').style.borderRadius="50px";
+	document.getElementById('IdIMG').style.onMouseOut="70px";
+
+}
